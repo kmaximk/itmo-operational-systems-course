706e105bdefd1c6a497e498030956237dc93ee49
Maxim <entpfc@mail.ru>
2023-09-28 17:03:12 +0300

    fa

--- user/pingpong.c
+++ user/pingpong.c
@@ -15,6 +15,13 @@ void read_from_pipe(char *buf, int fd) {
   close(fd);
 }
 
+void write_to_pipe(char *msg, int fd) {
+  if (write(fd, msg, sizeof(msg)) < sizeof(msg)) {
+    fprintf(2, "Error when writing\n");
+    exit(1);
+  }
+  close(fd);
+}
 int main(int argc, char *argv[]) {
   int fd1[2];
   int fd2[2];
@@ -28,22 +35,12 @@ int main(int argc, char *argv[]) {
     close(fd1[1]);
     close(fd2[0]);
     read_from_pipe(buf, fd1[0]);
-    char msg[5] = "pong\n";
-    if (write(fd2[1], msg, sizeof(msg)) < sizeof(msg)) {
-      fprintf(2, "Error when writing\n");
-      exit(1);
-    }
-    close(fd2[1]);
+    write_to_pipe("pong\n", fd2[1]);
     exit(0);
   } else if (pid > 0) {
     close(fd2[1]);
     close(fd1[0]);
-    char msg[5] = "ping\n";
-    if (write(fd1[1], msg, sizeof(msg)) < sizeof(msg)) {
-      fprintf(2, "Error when writing\n");
-      exit(1);
-    }
-    close(fd1[1]);
+    write_to_pipe("ping\n", fd1[1]);
     wait(0);
     read_from_pipe(buf, fd2[0]);
     exit(0);
